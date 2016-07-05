96fa755280c7e1a3039aa87e50f3b4f8fb0e784f
Jaehyun Lim <lim0606@gmail.com>
2016-07-05 10:10:13 +0900

    update MaskedParallelCriterion example

--- test/test_masked_parallel_criterion.lua
+++ test/test_masked_parallel_criterion.lua
@@ -11,7 +11,7 @@ local fw = 4
 
 local rpn_CE    = objrcg.MaskedSpatialCrossEntropyCriterion()
 local rpn_SL1   = objrcg.MaskedSmoothL1Criterion()
-local frcnn_CE  = objrcg.MaskedSpatialCrossEntropyCriterion()
+local frcnn_CE  = objrcg.MaskedCrossEntropyCriterion()
 local frcnn_SL1 = objrcg.MaskedSmoothL1Criterion()
 local criterion = objrcg.MaskedParallelCriterion():add(rpn_CE)
                                                   :add(rpn_SL1)
@@ -21,21 +21,21 @@ criterion:cuda()
 
 local input = {}
 input[1] = torch.rand(batch_size, n_classes, fh, fw):cuda()
-input[2] = torch.rand(batch_size, channels, fh, fw):cuda()
-input[3] = torch.rand(batch_size, n_classes, fh, fw):cuda()
-input[4] = torch.rand(batch_size, channels, fh, fw):cuda()
+input[2] = torch.rand(batch_size, n_classes * channels, fh, fw):cuda()
+input[3] = torch.rand(batch_size, n_classes):cuda()
+input[4] = torch.rand(batch_size, n_classes * channels):cuda()
 
 local target = {}
 target[1] = torch.zeros(batch_size, fh, fw):random(1, n_classes):cuda()
-target[2] = torch.rand(batch_size, channels, fh, fw):cuda()
-target[3] = torch.rand(batch_size, fh, fw):random(1, n_classes):cuda()
-target[4] = torch.rand(batch_size, channels, fh, fw):cuda()
+target[2] = torch.rand(batch_size, n_classes * channels, fh, fw):cuda()
+target[3] = torch.rand(batch_size):random(1, n_classes):cuda()
+target[4] = torch.rand(batch_size, n_classes * channels):cuda()
 
 local mask = {}
 mask[1] = torch.zeros(batch_size, fh, fw):random(0, 1):cuda()
-mask[2] = torch.zeros(batch_size, channels, fh, fw):random(0, 1):cuda()
+mask[2] = torch.zeros(batch_size, n_classes * channels, fh, fw):random(0, 1):cuda()
 mask[3] = nil 
-mask[4] = torch.zeros(batch_size, channels, fh, fw):random(0, 1):cuda()
+mask[4] = torch.zeros(batch_size, n_classes * channels):random(0, 1):cuda()
 
 local loss = criterion:forward(input, target, mask)
 print(loss)
